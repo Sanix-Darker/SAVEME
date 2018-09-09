e817b151db86d6c6e59c4c3cc415624c1ef9d664
Sanix-Darker <sanix000@yahoo.fr>
2018-09-09 03:49:21 +0200

    Date frequency is ok, this versions seens to be stable

--- ToFile.php
+++ ToFile.php
@@ -3,9 +3,6 @@
 set_time_limit(0);
 ignore_user_abort(true);
 
-require 'config.php';
-require 'classSAVEME.php';
-
 $time = -microtime(true);
 
 $dump = new SAVEME(new mysqli($host, $user, $password , $database));
--- index.php
+++ index.php
@@ -1,12 +1,30 @@
 <?php 
 	//SAVEME PHP Version 1.0
 	// this is the main file for managing the frequency
-
+	require 'config.php';
+	require 'classSAVEME.php';
 
 	if(isset($_REQUEST['now'])){ // this is for an instant backup
-
+		include('ToFile.php');
 	}else{
 
+		// $time_between_backups
+		$myFile = "date_NEVER_DELETE_THIS_FILE.txt";
+		$myFileLink = fopen($myFile, 'r');
+		$myFileContents = fread($myFileLink, filesize($myFile));
+		fclose($myFileLink);
+		$d1 = explode(' ', $myFileContents)[0];
+		$d2 = date('Y-m-d');
+
+		$date1 = date_create($d1);
+		$date2 = date_create($d2);
+
+		//difference between two dates
+		$diff = date_diff($date1,$date2);
+
+		if ($diff->format("%a") >= $time_between_backups){
+			include('ToFile.php');
+		}
 	}
 
 ?>
