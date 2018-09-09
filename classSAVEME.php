4940ba2a13bdef8547269fba50dd7dd6a1acdc08
Sanix-Darker <sanix000@yahoo.fr>
2018-09-09 03:06:21 +0200

    The magical dum function that i will use to passfrom the table to a logical file

--- classSAVEME.php
+++ classSAVEME.php
@@ -42,6 +42,96 @@ class SAVEME
 		}
 	}
 
+	/**
+	 * Dumps table to logical file.
+	 * @param  resource
+	 * @return void
+	 */
+	public function dumpTable($handle, $table)
+	{
+		$delTable = $this->delimite($table);
+		$res = $this->connection->query("SHOW CREATE TABLE $delTable");
+		$row = $res->fetch_assoc();
+		$res->close();
+
+		fwrite($handle, "-- --------------------------------------------------------\n\n");
+
+		$mode = isset($this->tables[$table]) ? $this->tables[$table] : $this->tables['*'];
+		$view = isset($row['Create View']);
+
+		if ($mode & self::DROP) {
+			fwrite($handle, 'DROP ' . ($view ? 'VIEW' : 'TABLE') . " IF EXISTS $delTable;\n\n");
+		}
+
+		if ($mode & self::CREATE) {
+			fwrite($handle, $row[$view ? 'Create View' : 'Create Table'] . ";\n\n");
+		}
+
+		if (!$view && ($mode & self::DATA)) {
+			$numeric = array();
+			$res = $this->connection->query("SHOW COLUMNS FROM $delTable");
+			$cols = array();
+			while ($row = $res->fetch_assoc()) {
+				$col = $row['Field'];
+				$cols[] = $this->delimite($col);
+				$numeric[$col] = (bool) preg_match('#^[^(]*(BYTE|COUNTER|SERIAL|INT|LONG$|CURRENCY|REAL|MONEY|FLOAT|DOUBLE|DECIMAL|NUMERIC|NUMBER)#i', $row['Type']);
+			}
+			$cols = '(' . implode(', ', $cols) . ')';
+			$res->close();
+
+
+			$size = 0;
+			$res = $this->connection->query("SELECT * FROM $delTable", MYSQLI_USE_RESULT);
+			while ($row = $res->fetch_assoc()) {
+				$s = '(';
+				foreach ($row as $key => $value) {
+					if ($value === null) {
+						$s .= "NULL,\t";
+					} elseif ($numeric[$key]) {
+						$s .= $value . ",\t";
+					} else {
+						$s .= "'" . $this->connection->real_escape_string($value) . "',\t";
+					}
+				}
+
+				if ($size == 0) {
+					$s = "INSERT INTO $delTable $cols VALUES\n$s";
+				} else {
+					$s = ",\n$s";
+				}
+
+				$len = strlen($s) - 1;
+				$s[$len - 1] = ')';
+				fwrite($handle, $s, $len);
+
+				$size += $len;
+				if ($size > self::MAX_SQL_SIZE) {
+					fwrite($handle, ";\n");
+					$size = 0;
+				}
+			}
+
+			$res->close();
+			if ($size) {
+				fwrite($handle, ";\n");
+			}
+			fwrite($handle, "\n");
+		}
+
+		if ($mode & self::TRIGGERS) {
+			$res = $this->connection->query("SHOW TRIGGERS LIKE '" . $this->connection->real_escape_string($table) . "'");
+			if ($res->num_rows) {
+				fwrite($handle, "DELIMITER ;;\n\n");
+				while ($row = $res->fetch_assoc()) {
+					fwrite($handle, "CREATE TRIGGER {$this->delimite($row['Trigger'])} $row[Timing] $row[Event] ON $delTable FOR EACH ROW\n$row[Statement];;\n\n");
+				}
+				fwrite($handle, "DELIMITER ;\n\n");
+			}
+			$res->close();
+		}
+
+		fwrite($handle, "\n");
+	}
 
 	private function delimite($s)
 	{
