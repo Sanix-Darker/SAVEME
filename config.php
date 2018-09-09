c0f3fde270f6feb46c70f562eb60e92c78d4f590
Sanix-Darker <sanix000@yahoo.fr>
2018-09-09 03:00:23 +0200

    Config file and backup repository for the databases..

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php 
+	//All config about the database
+	
+	$host = "localhost";
+	$database = "database";
+	$user = "root";
+	$password = "";
+	$time_between_backups = 1; // 1 represent here a day!
+?>
